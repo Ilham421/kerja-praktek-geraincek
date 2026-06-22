3c9e9be84939fb5879445d6ed6eb883ff012eec3
Ilham421 <ilhammohamad614@gmail.com>
2026-06-22 07:43:42 +0700

    update

--- src/app/kontak/page.jsx
+++ src/app/kontak/page.jsx
@@ -13,7 +13,7 @@ export default function KontakPage() {
   useEffect(() => {
     AOS.init({
       duration: 600,
-      once: false,
+      once: true,
       easing: "ease-out",
       offset: 50,
     });

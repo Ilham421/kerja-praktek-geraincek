221b86d4269ad6d2700b9c45903ecf1872d17feb
Ilham421 <ilhammohamad614@gmail.com>
2026-06-23 11:16:28 +0700

    update

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,11 +1,17 @@
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3306,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 export default db;
